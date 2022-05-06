20a3a17bd17c8ba148d8e2745c09af6a35b5ae70
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-05-06 13:01:17 +0100

    Add basic arithmetic functions

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+console.log(add(2, 5));
+
+const subtract = (a, b) => a - b;
+console.log(subtract(2,5));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(2,5));
+
+const divide = (a, b) => a / b;
+console.log(divide(2, 5));
