b2752cd23187b1a4af9a05013907317a03d5ec74
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-05-16 12:19:58 +0100

    Add function to display numeric input
    Add functions to display both numeric and decimal input.
    When display character limit is reached no more updates are made to the value.
    Decimal places are listened for only once.

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
 
+
+
 function operate(sum, a, b) {
     if (sum === add) return add(a, b);
     if (sum === subtract) return subtract(a, b);
@@ -13,4 +15,21 @@ function operate(sum, a, b) {
     if (sum === divide) return divide(a, b);
 }
 
-console.log(operate(add, 2, 5));
+
+const numberKeys = document.querySelectorAll('.numeral');
+numberKeys.forEach((key) => {
+    key.addEventListener('click', () => {
+        const displayValue = document.querySelector('.screen5').textContent;
+        if(displayValue.length < 9) {
+            document.querySelector('.screen5').textContent += key.value;
+        }
+        console.log(displayValue.length);
+    }, false);
+});
+
+
+const decimalKey = document.querySelector('.decimal');
+    decimalKey.addEventListener('click', () => {
+        document.querySelector('.screen5').textContent += decimalKey.value;
+    }, { once: true }); 
+
