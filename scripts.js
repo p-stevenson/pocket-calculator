b31d0093522525ec1ace87b4a8cb85731256520f
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-05-23 10:58:53 +0100

    Add listen for new operator function

--- scripts.js
+++ scripts.js
@@ -1,35 +1,138 @@
+let a;
+let b;
+let result;
+let operatorType;
+
+const display = document.querySelector('.screen5');
+const numberKeys = document.querySelectorAll('.numeral');
+const decimalKey = document.querySelector('.decimal');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+
+
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
-
 const divide = (a, b) => a / b;
 
 
+function clearButton() {
+    document.querySelector('.clear').addEventListener('click', clear, false);
+}
 
-function operate(sum, a, b) {
-    if (sum === add) return add(a, b);
-    if (sum === subtract) return subtract(a, b);
-    if (sum === multiply) return multiply(a, b);
-    if (sum === divide) return divide(a, b);
+function clear() {
+    a = '';
+    b = '';
+    result = 0;
+    operatorType = '';
+    display.textContent = '';
+}
+
+function clearScreen() {
+    display.textContent = '';
 }
 
 
-const numberKeys = document.querySelectorAll('.numeral');
-numberKeys.forEach((key) => {
-    key.addEventListener('click', () => {
-        const displayValue = document.querySelector('.screen5').textContent;
-        if(displayValue.length < 9) {
-            document.querySelector('.screen5').textContent += key.value;
-        }
-        console.log(displayValue.length);
-    }, false);
-});
+function listenForA() {
+    numberKeys.forEach((key) => {
+        key.addEventListener('click', storeA, false);
+    });
+}
+
+function listenForB() {
+    operators.forEach((operator) => {
+        operator.removeEventListener('click', storeOperatorType, false);
+    });
+    numberKeys.forEach((key) => {
+        key.addEventListener('click', storeB, false);
+    });
+}
+
+function storeA() {
+    if(display.textContent.length < 9) {
+        display.textContent += this.value;
+        a = Number(display.textContent);
+    }
+    listenForOperator();
+    listenForEquals();
+}
+
+function storeB() {
+    if(display.textContent.length < 9) {
+        display.textContent += this.value;
+        b = Number(display.textContent);
+    }
+    listenForOperator();
+    listenForEquals();
+}
+
+function listenForOperator() {
+    operators.forEach((operator) => {
+        operator.addEventListener('click', storeOperatorType, false);
+    });
+}
+
+function storeOperatorType() {
+    numberKeys.forEach((key) => {
+        key.removeEventListener('click', storeA, false)
+    });
+    numberKeys.forEach((key) => {
+        key.removeEventListener('click', storeB, false)
+    });
+    operatorType = this.value;
+    clearScreen();
+    if (b === "" || b ==='0'){
+        listenForB();
+    } else {
+        operate();
+    }
+}
+
+function operate() {
+    if (operatorType === 'add') {
+        result = add(a,b);
+        a = result;
+        b = '';
+        display.textContent = result;
+        listenForOperator();
+    }
+    if (operatorType === 'subtract') {
+        result = subtract(a, b);
+        a = result;
+        b = '';
+        display.textContent = result;
+        listenForOperator();
+    }
+    if (operatorType === multiply) return multiply(a, b);
+    if (operatorType === divide) return divide(a, b);
+}
+
+function listenForEquals() {
+    equals.addEventListener('click', equalsOperation, false);
+}
+
+function equalsOperation() {
+    if (b === '' || b === '0') {
+        display.textContent = 'empty';
+    } else if (operatorType === 'add') {
+        result = add(a, b);
+        a = result;
+        display.textContent = result;
+        listenForOperator();
+    } else if (operatorType === 'subtract') {
+        result = subtract(a, b);
+        a = result;
+        display.textContent = result;
+        listenForOperator();
+    }
+}
 
 
-const decimalKey = document.querySelector('.decimal');
-    decimalKey.addEventListener('click', () => {
-        document.querySelector('.screen5').textContent += decimalKey.value;
-    }, { once: true }); 
+/* 
+Notes: 
+*/
+
+clear();
+clearButton();
+listenForA();
 
