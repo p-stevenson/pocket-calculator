7bd25a4f344d9ce9eba06c1b706c87b01d0216f2
Peter Stevenson <54287958+twofivetwo@users.noreply.github.com>
2022-05-25 14:16:35 +0100

    Add basic calculator functionality

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 let a;
 let b;
-let result;
-let operatorType;
+let c;
+let firstOperatorType;
+let secondOperatorType;
 
 const display = document.querySelector('.screen5');
+const topDisplay = document.querySelector('.screen4');
+const errorDisplay = document.querySelector('.screen1');
 const numberKeys = document.querySelectorAll('.numeral');
 const decimalKey = document.querySelector('.decimal');
 const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('.equals');
+
 
 
 const add = (a, b) => a + b;
@@ -17,34 +21,49 @@ const divide = (a, b) => a / b;
 
 
 function clearButton() {
+    numberKeys.forEach((key) => {
+        key.removeEventListener('click', storeA, false)
+    });
+    numberKeys.forEach((key) => {
+        key.removeEventListener('click', storeB, false)
+    });
+    numberKeys.forEach((key) => {
+        key.removeEventListener('click', storeNewB, false)
+    });
+    operators.forEach((operator) => {
+        operator.removeEventListener('click', storeFirstOperatorType, false);
+    });
+    operators.forEach((operator) => {
+        operator.removeEventListener('click', storeSecondOperatorType, false);
+    });
+    operators.forEach((operator) => {
+        operator.removeEventListener('click', storeFinalOperatorType, false);
+    });
     document.querySelector('.clear').addEventListener('click', clear, false);
 }
 
 function clear() {
-    a = '';
-    b = '';
-    result = 0;
-    operatorType = '';
+    a = 0;
+    b = 0;
+    c = 0;
+    firstOperatorType = '';
+    secondOperatorType = '';
     display.textContent = '';
+    topDisplay.textContent = '';
+    errorDisplay.textContent = '';
+    document.querySelector('.clear').removeEventListener('click', clear, false);
+    clearButton();
+    listenForA();
 }
 
 function clearScreen() {
     display.textContent = '';
 }
 
-
+//-------------------FIRST OPERATION---------------------//
 function listenForA() {
     numberKeys.forEach((key) => {
         key.addEventListener('click', storeA, false);
-    });
-}
-
-function listenForB() {
-    operators.forEach((operator) => {
-        operator.removeEventListener('click', storeOperatorType, false);
-    });
-    numberKeys.forEach((key) => {
-        key.addEventListener('click', storeB, false);
     });
 }
 
@@ -53,8 +72,32 @@ function storeA() {
         display.textContent += this.value;
         a = Number(display.textContent);
     }
-    listenForOperator();
-    listenForEquals();
+    listenForFirstOperator();
+}
+
+function listenForFirstOperator() {
+    operators.forEach((operator) => {
+        operator.addEventListener('click', storeFirstOperatorType, false);
+    });
+}
+
+function storeFirstOperatorType() {
+    numberKeys.forEach((key) => {
+        key.removeEventListener('click', storeA, false)
+    });
+    clearScreen();
+    topDisplay.textContent = `${a} ${this.textContent}`;
+    firstOperatorType = this.value;
+    listenForB();
+}
+
+function listenForB() {
+    operators.forEach((operator) => {
+        operator.removeEventListener('click', storeFirstOperatorType, false);
+    });
+    numberKeys.forEach((key) => {
+        key.addEventListener('click', storeB, false);
+    });
 }
 
 function storeB() {
@@ -62,77 +105,120 @@ function storeB() {
         display.textContent += this.value;
         b = Number(display.textContent);
     }
-    listenForOperator();
-    listenForEquals();
+    listenForSecondOperator();
 }
 
-function listenForOperator() {
+function listenForSecondOperator() {
     operators.forEach((operator) => {
-        operator.addEventListener('click', storeOperatorType, false);
+        operator.addEventListener('click', storeSecondOperatorType, false);
     });
 }
 
-function storeOperatorType() {
+function storeSecondOperatorType() {
     numberKeys.forEach((key) => {
-        key.removeEventListener('click', storeA, false)
+        key.removeEventListener('click', storeB, false);
+    });
+    secondOperatorType = this.value;
+    if(firstOperatorType === 'add') {
+        c = add(a, b);
+        display.textContent = c;
+        topDisplay.textContent = `${c} ${this.textContent}`;
+    }
+    if(firstOperatorType === 'subtract') {
+        c = subtract(a, b);
+        display.textContent = c;
+        topDisplay.textContent = `${c} ${this.textContent}`;
+    }
+    if(firstOperatorType === 'multiply') {
+        c = multiply(a, b);
+        display.textContent = c;
+        topDisplay.textContent = `${c} ${this.textContent}`;
+    }
+    if(firstOperatorType === 'divide') {
+        if(b !== 0) {
+            c = divide(a, b);
+            display.textContent = c;
+            topDisplay.textContent = `${c} ${this.textContent}`;  
+        } else {
+            errorDisplay.textContent = 'ERROR';
+        }
+
+    }
+    firstOperatorType = secondOperatorType;
+    listenForNewB();
+}
+
+//-------------------SECOND OPERATION---------------------//
+
+function listenForNewB() {
+    operators.forEach((operator) => {
+        operator.removeEventListener('click', storeSecondOperatorType, false);
+    });
+    operators.forEach((operator) => {
+        operator.removeEventListener('click', storeFinalOperatorType, false);
     });
     numberKeys.forEach((key) => {
-        key.removeEventListener('click', storeB, false)
+        key.addEventListener('click', clearScreenAndStopListening, {once:true});
     });
-    operatorType = this.value;
+    numberKeys.forEach((key) => {
+        key.addEventListener('click', storeNewB, false);
+    });
+}
+
+function storeNewB() {
+    if(display.textContent.length < 9) {
+        display.textContent += this.value;
+        b = Number(display.textContent);
+    }
+    listenForFinalOperator();
+}
+
+function clearScreenAndStopListening() {
     clearScreen();
-    if (b === "" || b ==='0'){
-        listenForB();
-    } else {
-        operate();
-    }
+    numberKeys.forEach((key) => {
+        key.removeEventListener('click', clearScreenAndStopListening, {once:true});
+    });
 }
 
-function operate() {
-    if (operatorType === 'add') {
-        result = add(a,b);
-        a = result;
-        b = '';
-        display.textContent = result;
-        listenForOperator();
-    }
-    if (operatorType === 'subtract') {
-        result = subtract(a, b);
-        a = result;
-        b = '';
-        display.textContent = result;
-        listenForOperator();
-    }
-    if (operatorType === multiply) return multiply(a, b);
-    if (operatorType === divide) return divide(a, b);
+function listenForFinalOperator() {
+    operators.forEach((operator) => {
+        operator.addEventListener('click', storeFinalOperatorType, false);
+    });
 }
 
-function listenForEquals() {
-    equals.addEventListener('click', equalsOperation, false);
-}
-
-function equalsOperation() {
-    if (b === '' || b === '0') {
-        display.textContent = 'empty';
-    } else if (operatorType === 'add') {
-        result = add(a, b);
-        a = result;
-        display.textContent = result;
-        listenForOperator();
-    } else if (operatorType === 'subtract') {
-        result = subtract(a, b);
-        a = result;
-        display.textContent = result;
-        listenForOperator();
+function storeFinalOperatorType() {
+    numberKeys.forEach((key) => {
+        key.removeEventListener('click', storeNewB, false);
+    });
+    topDisplay.textContent = this.value;
+    secondOperatorType = this.value;
+    if(firstOperatorType === 'add') {
+        c = add(c, b);
+        display.textContent = c;
+        topDisplay.textContent = `${c} ${this.textContent}`;
+        listenForNewB();
     }
+    if(firstOperatorType === 'subtract') {
+        c = subtract(c, b);
+        display.textContent = c;
+        topDisplay.textContent = `${c} ${this.textContent}`;
+        listenForNewB();
+    }
+    if(firstOperatorType === 'multiply') {
+        c = multiply(c, b);
+        display.textContent = c;
+        topDisplay.textContent = `${c} ${this.textContent}`;
+        listenForNewB();
+    }
+    if(firstOperatorType === 'divide') {
+        c = divide(c, b);
+        display.textContent = c;
+        topDisplay.textContent = `${c} ${this.textContent}`;
+        listenForNewB();
+    }
+    firstOperatorType = secondOperatorType;
 }
 
-
-/* 
-Notes: 
-*/
-
-clear();
 clearButton();
 listenForA();
 
